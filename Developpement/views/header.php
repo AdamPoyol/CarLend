8fb3a6061e2d971f3a20167a75708cdfb5d458a6
AdamPoyol <poyol.adam@gmail.com>
2018-04-12 12:30:43 +0200

    Modif = 1 css pour tout le site ; include du header en php

---
+++ Developpement/views/header.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<div class=\"header\">
+				<header>
+					<div class=\"header_gauche\">
+						<a href=\"accueil_carlend.html\"><img id=\"CarLend\" src=\"../Images/logo.png\" alt=\"logo CarLend\"/></a>
+					</div>
+					<div class=\"header_droite\">
+						<input id=\"login\" type=\"text\" name=\"login\" placeholder=\"login\"/>
+						<input id=\"mdp\" type=\"password\" name=\"mdp\" placeholder=\"password\"/>
+						<input type=\"submit\" value=\"Connexion\"/>
+						<input type=\"submit\" value=\"Inscription\"/><br>
+					</div>
+				</header>
+			</div>";
