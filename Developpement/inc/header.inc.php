ab5c0b5192ecf8ece6d57768e07073f8e5fac6b7
antoine acquart <antoine.acquart@ynov.com>
2018-04-12 13:46:28 +0200

    test

---
+++ Developpement/inc/header.inc.php
@@ -0,0 +1,21 @@
+<!Doctype html>
+<html lang="fr">
+    <head>
+        <title></title>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="<?php echo RACINE_SITE; ?>inc/css/style.css"/>
+    </head>
+    <body>
+        <header>
+            <div class="conteneur">
+                <div><a href="" title="MonSite">MonSite.com</a></div>
+            </div>
+            <nav>
+                <a href="<?php echo RACINE_SITE; ?>inscription.php">inscription</a>
+                <a href="<?php echo RACINE_SITE; ?>connexion.php">connexion</a>
+                <a href="<?php echo RACINE_SITE; ?>boutique.php">boutique</a>
+                <a href="<?php echo RACINE_SITE; ?>panier.php">panier</a>
+            </nav>
+        </header>
+        <section>
+            <div class="conteneur">
