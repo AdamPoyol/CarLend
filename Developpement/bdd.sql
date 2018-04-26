ad0c2f7e0ebdd1f157bf038e72389daa9be405ae
Warmachine83 <romainmesguich@gmail.com>
2018-04-26 15:56:14 +0200

    modif sql

--- Developpement/bdd.sql
+++ Developpement/bdd.sql
@@ -1,3 +1,12 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Table: Locataire
+#------------------------------------------------------------
+
 CREATE TABLE Locataire(
         id_utilisateur    int (11) Auto_increment  NOT NULL ,
         Nom               Varchar (50) NOT NULL ,
@@ -18,6 +27,11 @@ CREATE TABLE Locataire(
         PRIMARY KEY (id_utilisateur )
 )ENGINE=InnoDB;
 
+
+#------------------------------------------------------------
+# Table: véhicule
+#------------------------------------------------------------
+
 CREATE TABLE vehicule(
         id_vehicule              int (11) Auto_increment  NOT NULL ,
         marque                   Varchar (50) NOT NULL ,
@@ -36,35 +50,56 @@ CREATE TABLE vehicule(
         PRIMARY KEY (id_vehicule )
 )ENGINE=InnoDB;
 
+
+#------------------------------------------------------------
+# Table: facture
+#------------------------------------------------------------
+
 CREATE TABLE facture(
         id_facture     int (11) Auto_increment  NOT NULL ,
-        id_utilisateur int (11) Auto_increment  NOT NULL ,
-        id_voiture     int (11) Auto_increment  NOT NULL ,
+        id_utilisateur Int NOT NULL ,
+        id_voiture     Int NOT NULL ,
         date_location  Date NOT NULL ,
         date_retour    Date NOT NULL ,
         prix           Int NOT NULL ,
+        id_tarif       Int NOT NULL ,
         PRIMARY KEY (id_facture )
 )ENGINE=InnoDB;
 
+
+#------------------------------------------------------------
+# Table: historique
+#------------------------------------------------------------
+
 CREATE TABLE historique(
-        id_utilisateur int (11) Auto_increment  NOT NULL ,
-        id_location    int (11) Auto_increment  NOT NULL ,
-        id_facture     int (11) Auto_increment  NOT NULL ,
-        id_vehicule    int (11) Auto_increment  NOT NULL ,
+        id_utilisateur Int NOT NULL ,
+        id_location    Int NOT NULL ,
+        id_facture     Int NOT NULL ,
+        id_vehicule    Int NOT NULL ,
         id_facture_1   Int NOT NULL ,
         PRIMARY KEY (id_utilisateur )
 )ENGINE=InnoDB;
 
+
+#------------------------------------------------------------
+# Table: tarif
+#------------------------------------------------------------
+
 CREATE TABLE tarif(
-        id_devis          int (11) Auto_increment  NOT NULL ,
+        id_tarif          int (11) Auto_increment  NOT NULL ,
         modele            Varchar (50) NOT NULL ,
         marque            Varchar (50) NOT NULL ,
         Puissance         Int NOT NULL ,
         Puissance_fiscale Int NOT NULL ,
         annee_vehicule    Date NOT NULL ,
         prix              Int NOT NULL ,
-        PRIMARY KEY (id_devis )
+        PRIMARY KEY (id_tarif )
 )ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: créé des infos de connexion
+#------------------------------------------------------------
 
 CREATE TABLE cree_des_infos_de_connexion(
         id_vehicule Int NOT NULL ,
@@ -72,10 +107,15 @@ CREATE TABLE cree_des_infos_de_connexion(
         PRIMARY KEY (id_vehicule ,id_facture )
 )ENGINE=InnoDB;
 
+
+#------------------------------------------------------------
+# Table: créé
+#------------------------------------------------------------
+
 CREATE TABLE cree(
-        id_devis   Int NOT NULL ,
+        id_tarif   Int NOT NULL ,
         id_facture Int NOT NULL ,
-        PRIMARY KEY (id_devis ,id_facture )
+        PRIMARY KEY (id_tarif ,id_facture )
 )ENGINE=InnoDB;
 
 ALTER TABLE Locataire ADD CONSTRAINT FK_Locataire_id_facture FOREIGN KEY (id_facture) REFERENCES facture(id_facture);
@@ -83,5 +123,5 @@ ALTER TABLE vehicule ADD CONSTRAINT FK_vehicule_id_utilisateur_Locataire FOREIGN
 ALTER TABLE historique ADD CONSTRAINT FK_historique_id_facture_1 FOREIGN KEY (id_facture_1) REFERENCES facture(id_facture);
 ALTER TABLE cree_des_infos_de_connexion ADD CONSTRAINT FK_cree_des_infos_de_connexion_id_vehicule FOREIGN KEY (id_vehicule) REFERENCES vehicule(id_vehicule);
 ALTER TABLE cree_des_infos_de_connexion ADD CONSTRAINT FK_cree_des_infos_de_connexion_id_facture FOREIGN KEY (id_facture) REFERENCES facture(id_facture);
-ALTER TABLE cree ADD CONSTRAINT FK_cree_id_devis FOREIGN KEY (id_devis) REFERENCES tarif(id_devis);
+ALTER TABLE cree ADD CONSTRAINT FK_cree_id_tarif FOREIGN KEY (id_tarif) REFERENCES tarif(id_tarif);
 ALTER TABLE cree ADD CONSTRAINT FK_cree_id_facture FOREIGN KEY (id_facture) REFERENCES facture(id_facture);
