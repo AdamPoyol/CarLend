71260c3590731af3a3220387158782fb8eb9d510
antoine acquart <antoine.acquart@ynov.com>
2018-04-26 16:50:09 +0200

    26/04 16h50

--- Developpement/bdd.sql
+++ Developpement/bdd.sql
@@ -4,6 +4,7 @@ USE carlend;
 CREATE TABLE IF NOT EXISTS utilisateur(
   id_utilisateur INT UNSIGNED AUTO_INCREMENT,
   mot_de_passe VARCHAR(32) NOT NULL,
+  identifiant VARCHAR(30) NOT NULL,
   nom VARCHAR(20) NOT NULL,
   prenom VARCHAR(20) NOT NULL,
   date_de_naissance DATE NOT NULL,
@@ -23,16 +24,16 @@ CREATE TABLE IF NOT EXISTS vehicule(
   id_vehicule INT UNSIGNED AUTO_INCREMENT,
   id_utilisateur INT UNSIGNED NULL DEFAULT NULL,
   immatriculation VARCHAR(10) NOT NULL,
-  marque VARCHAR(30) NOT NULL ,
-  modele VARCHAR(30) NOT NULL ,
+  marque VARCHAR(30) NOT NULL,
+  modele VARCHAR(30) NOT NULL,
   lien_photo VARCHAR(250),
-  puissance INT NOT NULL ,
-  puissance_fiscale INT NOT NULL ,
-  energie VARCHAR (25) NOT NULL ,
-  boite_vitesse VARCHAR (25) NOT NULL ,
-  nb_porte INT NOT NULL ,
-  nb_place INT NOT NULL ,
-  date_mise_en_location DATE NOT NULL ,
+  puissance INT NOT NULL,
+  puissance_fiscale INT NOT NULL,
+  energie VARCHAR (25) NOT NULL,
+  boite_vitesse VARCHAR (25) NOT NULL,
+  nb_porte INT NOT NULL,
+  nb_place INT NOT NULL,
+  date_mise_en_location DATE NOT NULL,
   CONSTRAINT pk_vehicule PRIMARY KEY(id_vehicule)
 )ENGINE=INNODB DEFAULT CHARSET=utf8;
 
@@ -47,26 +48,18 @@ CREATE TABLE IF NOT EXISTS facture(
   CONSTRAINT pk_facture PRIMARY KEY(id_facture)
 )ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-CREATE TABLE historique(
-  id_utilisateur INT UNSIGNED NULL DEFAULT NULL,
-  id_vehicule INT UNSIGNED NULL DEFAULT NULL,
-  id_facture     Int NOT NULL ,
-
-)ENGINE=InnoDB;
-
 CREATE TABLE tarif(
-        id_tarif          int (11) Auto_increment  NOT NULL ,
-        modele            Varchar (50) NOT NULL ,
-        marque            Varchar (50) NOT NULL ,
-        Puissance         Int NOT NULL ,
-        Puissance_fiscale Int NOT NULL ,
-        annee_vehicule    Date NOT NULL ,
-        prix              Int NOT NULL ,
-        PRIMARY KEY (id_tarif )
-)ENGINE=InnoDB;
+  id_tarif INT UNSIGNED AUTO_INCREMENT,
+  marque VARCHAR(30) NOT NULL,
+  modele VARCHAR(30) NOT NULL,
+  puissance INT NOT NULL,
+  puissance_fiscale INT NOT NULL,
+  annee_vehicule DATE NOT NULL,
+  prix INT NOT NULL,
+  CONSTRAINT pk_tarif PRIMARY KEY(id_tarif)
+)ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-ALTER TABLE Locataire ADD CONSTRAINT FK_Locataire_id_facture FOREIGN KEY (id_facture) REFERENCES facture(id_facture);
-ALTER TABLE vehicule ADD CONSTRAINT FK_vehicule_id_utilisateur_Locataire FOREIGN KEY (id_utilisateur_Locataire) REFERENCES Locataire(id_utilisateur);
-ALTER TABLE historique ADD CONSTRAINT FK_historique_id_facture_1 FOREIGN KEY (id_facture_1) REFERENCES facture(id_facture);
-ALTER TABLE cree_des_infos_de_connexion ADD CONSTRAINT FK_cree_des_infos_de_connexion_id_vehicule FOREIGN KEY (id_vehicule) REFERENCES vehicule(id_vehicule);
-ALTER TABLE cree_des_infos_de_connexion ADD CONSTRAINT FK_cree_des_infos_de_connexion_id_facture FOREIGN KEY (id_facture) REFERENCES facture(id_facture);
+ALTER TABLE vehicule ADD CONSTRAINT fk_vehicule_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur);
+ALTER TABLE facture ADD CONSTRAINT fk_facture_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur);
+ALTER TABLE facture ADD CONSTRAINT fk_facture_vehicule FOREIGN KEY (id_vehicule) REFERENCES vehicule(id_vehicule);
+ALTER TABLE facture ADD CONSTRAINT fk_facture_tarif FOREIGN KEY (id_tarif) REFERENCES tarif(id_tarif);
